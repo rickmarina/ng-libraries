5ca8e2314c2dd89475c6e5646c618109e2ef51e3
Rick Marina <rickmarina@gmail.com>
2025-08-12 23:09:14 +0200

    copied toast

--- projects/test-app/src/app/code/code.component.ts
+++ projects/test-app/src/app/code/code.component.ts
@@ -12,34 +12,29 @@ export class CodeComponent {
 
   @Input() title: string = '';
   @Input() innerCode: string = "";
-  @Input() run : boolean = true;
+  @Input() run: boolean = true;
 
   constructor(private _toastyService: ToastyService) {
   }
 
-  // executeAngularCode() {
-  //   try {
-  //     // eslint-disable-next-line no-eval
-  //     eval(this.innerCode);
-  //   } catch (error) {
-  //     console.error('Error executing snippet code:', error);
-  //   }
-  // }
-
-executeCode() {
-  try {
-    // Creamos una función que recibe el cuerpo del código tal cual
-    const func = new Function('_toastyService', 'ToastType', `
-        ${this.innerCode.replace(/this\._toastyService/g, '_toastyService')};
-      `); 
-
-    // Llamamos a la función con el contexto del componente
-    func(this._toastyService, ToastType);
-
-    console.log('Código ejecutado correctamente');
-  } catch (error) {
-    console.error('Error ejecutando código:', error);
+  copy() {
+    this._toastyService.showToast('Copied', '', { type: ToastType.Info});
   }
-}
-  
+
+  executeCode() {
+    try {
+      // Creamos una función que recibe el cuerpo del código tal cual
+      const func = new Function('_toastyService', 'ToastType', `
+        ${this.innerCode.replace(/this\._toastyService/g, '_toastyService')};
+      `);
+
+      // Llamamos a la función con el contexto del componente
+      func(this._toastyService, ToastType);
+
+      // console.log('Código ejecutado correctamente');
+    } catch (error) {
+      console.error('Error executing code:', error);
+    }
+  }
+
 }
